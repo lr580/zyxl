1473183e5bdcbb586189cde527bc5ea344f78a28
lr580 <2277379149@qq.com>
2021-08-30 16:15:45 +0800

    增加了部分内容

--- miniprogram/pages/postt/postt.js
+++ miniprogram/pages/postt/postt.js
@@ -5,14 +5,22 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    types: [],
+    id: '',
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    this.init_post(options);
+  },
 
+  //初始化
+  init_post: function (options) {
+    this.setData({
+      types: km.globalData.type_p,
+    });
   },
 
   /**
