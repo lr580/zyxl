488b6e848372c43fff6b8a2f1c12afdfe780fec5
YoungJiaxin <1797917338@qq.com>
2021-09-21 11:34:52 +0800

    新增页面Ui

--- miniprogram/pages/myhistory/myhistory.js
+++ miniprogram/pages/myhistory/myhistory.js
@@ -5,6 +5,9 @@ Page({
    * 页面的初始数据
    */
   data: {
+    TabCur: 0,
+    scrollLeft:0,
+    collect_type:['视频','树洞']
 
   },
 
@@ -15,7 +18,22 @@ Page({
     //该页面js代码使用批量生产，具体见app.js的init_pair_pagejs函数内的注释，以查看data和其他Page函数及其说明
     getApp().init_pair_pagejs(this, 'history', '浏览');
   },
+  tabSelect(e) {
+    this.setData({
+      TabCur: e.currentTarget.dataset.id,
+      scrollLeft: (e.currentTarget.dataset.id-1)*60
+    })
+    let newv = Number(e.currentTarget.dataset.id);
 
+      this.setData({
+        sele_bar: newv,
+      });
+      if (newv == 0) {
+        this.init_pfirst();
+      } else {
+        this.init_psecond();
+      }
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
