b117291dbe6f4b5e729089d84071c2b2a2e41eb2
scnu-yyyyjx <1797917338@qq.com>
2022-06-16 00:33:21 +0800

    新增答题页面ui 增强题目显示安全性

--- miniprogram/pages/questionbank/questionbank.js
+++ miniprogram/pages/questionbank/questionbank.js
@@ -1,18 +1,36 @@
 // pages/questionbank/questionbank.js
 import * as io from '../../js/common/io'
 import * as problem from '../../js/base/problemCtrl'
+const app = getApp();
+var now_type = 1;
 Page({
 
     /**
      * 页面的初始数据
      */
     data: {
-
+        TabCur: 0,
+        scrollLeft:0
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
+
+    click_type: function (v) {
+        console.log(v);
+         let tid = Number(v.currentTarget.dataset.id);
+         console.log(now_type);
+         console.log(tid);
+        this.setData({
+            TabCur: v.currentTarget.dataset.id,
+            scrollLeft: (v.currentTarget.dataset.id - 1) * 60,
+            now_type: tid
+        })
+        console.log(now_type);
+        console.log(tid);
+    },
+
     onLoad(options) {
         // problem.fitOptions(this, options);//移步onUpdate
         io.helpGotoParam(this, 'answersheet');
@@ -20,6 +38,9 @@ Page({
             combat: true,
         });
         problem.helpClearall(this);
+        this.setData({
+            now_type: 0
+        })
     },
 
     /**
