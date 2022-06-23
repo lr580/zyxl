af72c420a5516db3faa88556e8ead2fe5c464b1f
scnu-yyyyjx <1797917338@qq.com>
2022-06-23 22:58:07 +0800

    新增Ui

--- miniprogram/pages/questionbank/questionbank.js
+++ miniprogram/pages/questionbank/questionbank.js
@@ -3,12 +3,17 @@ import * as io from '../../js/common/io'
 import * as problem from '../../js/base/problemCtrl'
 const app = getApp();
 var now_type = 1;
+var startPoint;
 Page({
 
     /**
      * 页面的初始数据
      */
     data: {
+        buttonTop: 0,
+        buttonLeft: 0,
+        windowHeight: '',
+        windowWidth: '',
         TabCur: 0,
         scrollLeft:0
     },
@@ -16,6 +21,59 @@ Page({
     /**
      * 生命周期函数--监听页面加载
      */
+    onLoad: function (options) {
+        var that =this;
+        wx.getSystemInfo({
+          success: function (res) {
+            console.log(res);
+            // 屏幕宽度、高度
+            console.log('height=' + res.windowHeight);
+            console.log('width=' + res.windowWidth);
+            // 高度,宽度 单位为px
+            that.setData({
+              windowHeight:  res.windowHeight,
+              windowWidth:  res.windowWidth
+            })
+          }
+        })
+      },
+    
+      onShow: function () {
+       
+      },
+    
+      buttonStart: function (e) {
+        startPoint = e.touches[0]
+      },
+      buttonMove: function (e) {
+        var endPoint = e.touches[e.touches.length - 1]
+        var translateX = endPoint.clientX - startPoint.clientX
+        var translateY = endPoint.clientY - startPoint.clientY
+        startPoint = endPoint
+        var buttonTop = this.data.buttonTop + translateY
+        var buttonLeft = this.data.buttonLeft + translateX
+        //判断是移动否超出屏幕
+        if (buttonLeft+50 >= this.data.windowWidth){
+          buttonLeft = this.data.windowWidth-50;
+        }
+        if (buttonLeft<=0){
+          buttonLeft=0;
+        }
+        if (buttonTop<=0){
+          buttonTop=0
+        }
+        if (buttonTop + 50 >= this.data.windowHeight){
+          buttonTop = this.data.windowHeight-50;
+        }
+        this.setData({
+          buttonTop: buttonTop,
+          buttonLeft: buttonLeft
+        })
+      },
+      buttonEnd: function (e) {
+    
+      },
+    
 
     click_type: function (v) {
         console.log(v);
